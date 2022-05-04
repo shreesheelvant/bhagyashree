bfea2001e1d9402b9e45dd329acb920cedc6e200
unknown <suntanabhagyashree@gmail.com>
2022-05-04 09:15:06 +0530

    my first push

---
+++ copyfiles.sh
@@ -0,0 +1,10 @@
+for files in `ls *.txt`; 
+do 
+folderName= `echo $file | awk -F. '{print $1}'`;
+if [ -d $folderName ]
+then
+rm -R $folderName;
+fi
+mkdir $folderName;
+cp $file $folderName;
+done
